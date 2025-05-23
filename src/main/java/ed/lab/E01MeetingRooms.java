08b89ccf664ab88c4168b2094ba4e9ec81aba881
ll <als@gm>
2025-05-23 08:35:24 -0600

    .

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,42 @@
 package ed.lab;
 
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
+        if (meetingIntervals.isEmpty()) {
+            return 0;
+        }
+
+
+        /*
+        Queue<MeetingInterval> endTimeMinHeap = new PriorityQueue<>((a, b) -> a.endTime() - b.endTime());
+        Queue<MeetingInterval> endTimeMaxHeap = new PriorityQueue<>((a, b) -> b.endTime() - a.endTime());
+        Queue<MeetingInterval> startTimeMinHeap = new PriorityQueue<>((a, b) -> a.startTime() - b.startTime());
+        Queue<MeetingInterval> startTimeMaxHeap = new PriorityQueue<>((a, b) -> b.startTime() - a.startTime());
+
+        for (MeetingInterval interval : meetingIntervals) {
+            startTimeMinHeap.offer(interval);
+            endTimeMaxHeap.offer(interval);
+        }
+
+        int rooms = 0;
+        int time = 0;
+        while (!startTimeMinHeap.isEmpty()) {
+            MeetingInterval interval = startTimeMinHeap.poll();
+            if (interval.startTime() <= time && time <= interval.endTime()) {
+                endTimeMinHeap.offer(interval);
+
+            }
+            time++;
+        }
+
+        return rooms;
+
+         */
     }
 
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,21 @@
 package ed.lab;
 
+import java.util.HashMap;
+
 public class E02Logger {
+    HashMap<String, Integer> map;
 
     public E02Logger() {
+        this.map = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+        if (!map.containsKey(message)) {
+            map.put(message, timestamp + 10);
+            return true;
+        }
+
+        return map.get(message) <= timestamp;
     }
 
 }
